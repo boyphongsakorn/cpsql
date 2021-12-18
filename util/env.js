80fa503876ad8c9ea3795b7c59c8ff27fdc09fbd
Phongsakorn Wisetthon <boyphongsakorn@users.noreply.github.com>
2021-12-18 20:05:30 +0700

    Update env.js

--- util/env.js
+++ util/env.js
@@ -2,9 +2,9 @@ const env = {
     database: 'mysqlbase',
     username: 'root',
     password: 'Team1556th_',
-    host: '192.168.31.227',
-    //dialect: 'mysql',
-    dialect: 'mariadb',
+    host: '192.168.31.210',
+    dialect: 'mysql',
+    //dialect: 'mariadb',
     pool: {
       max: 5,
       min: 0,
