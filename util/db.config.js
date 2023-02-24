23464b6917ba4a3e719c33802ab5f51e51ea587a
Phongsakorn Wisetthon <boy1556@hotmail.com>
2023-02-24 11:26:46 +0700

    ok

--- util/db.config.js
+++ util/db.config.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 const env = require('./env');
-const sequelize = new Sequelize(env.database, env.username, env.password, {
-  host: env.host,
-  dialect: env.dialect,
+const sequelize = new Sequelize(env.database, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: 'mariadb',
   operatorsAliases: false,
 
   pool: {
